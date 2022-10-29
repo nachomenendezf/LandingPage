82c83fe7bfca9616938058dfe5e85869d19e249e
nachomenendezf <nachomenendezf@gmail.com>
2022-10-29 12:36:11 -0300

    Update

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
-/*function*/
+/*$(document).ready(function() {
+
+    var initialScroll = 0;
+
+    $(this).scroll(function() {
+        var lastPosition = $(this).scrollTop();
+
+        if (lastPosition < initialScroll) {
+            $('html, body').animate({
+                scrollTop: $(".first").position().top
+            }, 'slow', function(){
+                $('html, body').stop(true);
+            });
+        }else{
+            $('html, body').animate({
+                scrollTop: $(".second").position().top
+            }, 'slow', function(){
+                $('html, body').stop(true);
+            });
+        }
+
+        initialScroll = lastPosition;
+    });
+});*/
